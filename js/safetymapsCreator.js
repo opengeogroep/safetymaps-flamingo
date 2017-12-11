70b62411674a1e572d531b3ece119bf378e2f867
mvdstruijk <m_struijk1990@hotmail.nl>
2017-12-11 11:07:59 +0100

    unselect previous selected object and remove it from the map

--- js/safetymapsCreator.js
+++ js/safetymapsCreator.js
@@ -10,112 +10,128 @@ var safetymaps = safetymaps || {};
 safetymaps.safetymapsCreator = {
     conf: null,
     options: null,
-    objectLayers:null,
+    selectControl: null,
+    objectLayers: null,
     clusteringLayer: null,
-    
-    constructor: function(conf){
-        this.conf= conf;
+    selectedObject: null,
+    selectedClusterFeature: null,
+
+    constructor: function (conf) {
+        this.conf = conf;
         var me = this;
         this.options = $.extend({
-            clusteringSymbol:{
-                    icon: conf.imagePath+"cluster.png",
-                    width: 51,
-                    height: 56
+            clusteringSymbol: {
+                icon: conf.imagePath + "cluster.png",
+                width: 51,
+                height: 56
             },
-                objectSymbol:{ 
-                    icon: conf.imagePath+"object.png",
-                    width: 23,
-                    height: 40
-                }
+            objectSymbol: {
+                icon: conf.imagePath + "object.png",
+                width: 23,
+                height: 40
+            }
         }, this.options);
-        
+
         var pStyles = safetymaps.creator.api.getStyles();
         me.clusteringLayer = new safetymaps.ClusteringLayer(me.options);
         me.objectLayers = new safetymaps.creator.CreatorObjectLayers();
         //me.conf.map.getFrameworkMap().addLayers(me.objectLayers.createLayers());
-        
+
         me.createClusterLayer();
- 
+
     },
-    
-    //creates the clusterLayer with the features and the cluster strategy
-    createClusterLayer: function(){
+
+    //creates the clusterLayer with the features and register for feature/cluster clicked event
+    createClusterLayer: function () {
         var me = this;
         safetymaps.creator.api.getViewerObjectMapOverview()
-        .done(function(data) {
-            var features = safetymaps.creator.api.createViewerObjectFeatures(data);
-            
-            var clusterStrategy = new OpenLayers.Strategy.Cluster({distance:80, threshold:3});
-            me.clusteringLayer.createLayer();  
-            
-            clusterStrategy.setLayer(me.clusteringLayer.layer);
-            clusterStrategy.activate();
-            me.conf.map.getFrameworkMap().addLayer(me.clusteringLayer.layer);
-            me.clusteringLayer.addFeaturesToCluster(features);
-            
-            me.setClusterLayerControls(); 
-            
-            me.conf.map.getFrameworkMap().addLayers(me.objectLayers.createLayers());
-            $(me.clusteringLayer).on("object_selected", function(event, feature) {
-                me.clusterObjectSelected(feature);
-            });
-            
-        });
+                .done(function (data) {
+                    var features = safetymaps.creator.api.createViewerObjectFeatures(data);
+
+                    me.clusteringLayer.createLayer();
+
+                    me.conf.map.getFrameworkMap().addLayer(me.clusteringLayer.layer);
+                    me.clusteringLayer.addFeaturesToCluster(features);
+
+                    me.setClusterLayerControls();
+
+                    me.conf.map.getFrameworkMap().addLayers(me.objectLayers.createLayers());
+                    $(me.clusteringLayer).on("object_selected", function (event, feature) {
+                        me.clusterObjectSelected(feature);
+                    });
+                    $(me.clusteringLayer).on("object_cluster_selected", function (event, features) {
+                        me.clusterObjectClusterSelected(features);
+                    });
+
+                });
     },
-    
-    setClusterLayerControls: function(){
+
+    setClusterLayerControls: function () {
         var me = this;
-        var selectControl = new OpenLayers.Control.SelectFeature(
-            me.clusteringLayer.layer,
-            {
-                clickout: true,
-                toggle: true,
-                multiple: false
-            }
+        me.selectControl = new OpenLayers.Control.SelectFeature(
+                me.clusteringLayer.layer,
+                {
+                    clickout: true,
+                    toggle: true,
+                    multiple: false
+                }
         );
-        
-        me.conf.map.getFrameworkMap().addControl(selectControl);;
-        selectControl.activate();
-        
+
+        me.conf.map.getFrameworkMap().addControl(me.selectControl);
+        ;
+        me.selectControl.activate();
+
     },
-    
-    clusterObjectSelected: function(feature){
-        var me = this;
-        if(feature.cluster){
-            //build gui for search..
-        }else{
-            //single feature clicked
-            me.singleObjectClicked(feature);
-            
-        }
+
+    clusterObjectClusterSelected: function (feature) {
+        console.log("object_cluster_clicked", feature);
+
     },
-    
-    singleObjectClicked: function(feature){
+
+    clusterObjectSelected: function (feature) {
         var me = this;
+
+        // Unselect current, if any
+        me.unselectObject();
+
+        this.selectedClusterFeature = feature;
+
+        console.log("zooming to feature", feature.geometry);
         me.conf.map.getFrameworkMap().setCenter(new OpenLayers.LonLat(feature.geometry.x, feature.geometry.y), 13);
-        
+
         safetymaps.creator.api.getObjectDetails(feature.attributes.id)
-        .fail(function(msg) {
-            // TODO
-        })
-        .done(function(object) {
-            console.log(object);
-            me.selectedObjectDetailsReceived(object);
-        });
+                .fail(function (msg) {
+                    // TODO
+                })
+                .done(function (object) {
+                    me.selectedObjectDetailsReceived(object);
+                });
     },
-    
-    selectedObjectDetailsReceived: function(object){
+
+    unselectObject: function () {
+        if (this.selectedObject) {
+            this.objectLayers.removeAllFeatures();
+
+            if (this.selectedClusterFeature.layer) {
+                this.selectControl.unselect(this.selectedClusterFeature);
+            }
+        }
+        this.selectedObject = null;
+        this.selectedClusterFeature = null;
+    },
+
+    selectedObjectDetailsReceived: function (object) {
         var me = this;
         try {
             me.objectLayers.addFeaturesForObject(object);
             me.selectedObject = object;
-        } catch(error) {
+        } catch (error) {
             console.log("Error creating layers for object", object);
-            if(error.stack) {
+            if (error.stack) {
                 console.log(error.stack);
             }
         }
     }
-    
-    
+
+
 };
