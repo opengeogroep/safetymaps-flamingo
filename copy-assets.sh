f038525f4b8577bde97468576bd6464cb2c33ec8
mvdstruijk <m_struijk1990@hotmail.nl>
2018-01-08 12:03:36 +0100

    update

--- copy-assets.sh
+++ copy-assets.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-
 cp -R ../safetymaps-viewer/public/js/safetymaps/modules/creator/assets ../safetymaps-flamingo/
 cp -R ../safetymaps-viewer/public/css ../safetymaps-flamingo/
 cp -R ../safetymaps-viewer/public/locales ../safetymaps-flamingo/
 cp -R ../safetymaps-viewer/public/js/libs ../safetymaps-flamingo/
 
-ldconfig
-
-echo "Done!"
