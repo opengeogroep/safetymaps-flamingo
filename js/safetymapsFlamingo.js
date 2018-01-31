abf36e48e681d0dc57cb77f76c84e9650f06302f
mvdstruijk <m_struijk1990@hotmail.nl>
2018-01-31 15:30:32 +0100

    default language = NL

--- js/safetymapsFlamingo.js
+++ js/safetymapsFlamingo.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-var dbkjsLang = "en";
+var dbkjsLang = "nl";
 
 Ext.define("viewer.components.safetymapsFlamingo", {
     extend: "viewer.components.Component",
