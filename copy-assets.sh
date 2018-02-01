416fd27f1394cc1f2a6ef6a4f48723ee2e479659
mvdstruijk <m_struijk1990@hotmail.nl>
2018-02-01 17:33:55 +0100

    add delete links

--- copy-assets.sh
+++ copy-assets.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm -rf css locales libs
 cp -R ../safetymaps-viewer/public/js/safetymaps/modules/creator/assets ../safetymaps-flamingo/
 cp -R ../safetymaps-viewer/public/css ../safetymaps-flamingo/
 cp -R ../safetymaps-viewer/public/locales ../safetymaps-flamingo/
