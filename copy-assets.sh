4ac4227e3a6697ffd4b79b0c0f9d831b34d78274
mvdstruijk <m_struijk1990@hotmail.nl>
2018-12-21 13:46:11 +0100

    no longer needed

--- copy-assets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -rf css locales libs
-cp -R ../safetymaps-viewer/public/js/safetymaps/modules/creator/assets ../safetymaps-flamingo/
-cp -R ../safetymaps-viewer/public/css ../safetymaps-flamingo/
-cp -R ../safetymaps-viewer/public/locales ../safetymaps-flamingo/
-cp -R ../safetymaps-viewer/public/js/libs ../safetymaps-flamingo/
-
